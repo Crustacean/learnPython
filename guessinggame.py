9526b612b5979a6b21dbbbed5413098c7206182a
Crustacean <elkyem@gmail.com>
2025-03-27 19:46:58 +0300

    adding guessing game

--- guessinggame.py
+++ guessinggame.py
@@ -15,8 +15,10 @@ def get_user_input():
     answer = input("Who has more followers? Type 'A' or 'B': ").lower()
     return answer
 
-def swap():
-    print("here")
+def swap(data1, data2):
+    temp = data2
+    data1 = temp
+    return data1
 
 def compare(val1, val2, choice, total):
     print("compare")
@@ -25,7 +27,7 @@ def compare(val1, val2, choice, total):
         return total
     elif val1 < val2 and choice == val2:
         print(f"Sorry, that's wrong. Final score: {total}")
-        play_game = False
+        return total
 
 play_game = True
 
@@ -38,7 +40,7 @@ def play(option1):
         while option1 == None:
             option1 = getData()
 
-        print(option1["name"] + ", " + option1["description"] + ", " + option1["country"])
+        print(option1["name"] + ", " + option1["description"] + ", " + option1["country"], option1["follower count"])
 
         option2 = getData()
         while option2 == option1:
@@ -48,16 +50,18 @@ def play(option1):
         print("Vs")
         print("\n")
 
-        print(option2["name"] + ", " + option2["description"] + ", " + option2["country"])
+        print(option2["name"] + ", " + option2["description"] + ", " + option2["country"], option2["follower count"])
 
         a = option1["follower count"]
         b = option2["follower count"]
 
-        print(compare(a, b, get_user_input(), score))
+        score = compare(a, b, get_user_input(), score)
 
-        print(f"You're right! Current score: {score}.")
-
-        swap()
+        if score is not None:
+            print(f"You're right! Current score: {score}.")
+            option1 = swap(option1, option2)
+        else:
+            print("game over")
 
 
 play(option1)
