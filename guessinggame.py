bdd8de83e299327af049dda3ea3629a6e9c3d1d4
Crustacean <elkyem@gmail.com>
2025-03-27 21:03:34 +0300

    adding guessing game

--- guessinggame.py
+++ guessinggame.py
@@ -16,21 +16,19 @@ def get_user_input():
         else:
             print("Wrong input. Please type 'A' or 'B' ")
 
-def swap(data1, data2):
-    data1 = data2
-    return data1
-
 def compare(val1, val2, choice, total):
     if val1 > val2 and choice == val1:
-        print(f"You're right! Current score: {score}.")
         total += 1
+        print(f"You're right! Current score: {total}.")
         return total
     elif val1 < val2 and choice == val2:
         return total
     else:
         return total
 
-def play(score):
+def play():
+
+    global score
 
     play_game = True
     option1 = getData()
@@ -41,24 +39,24 @@ def play(score):
         while option2 == option1:
             option2 = getData()
         
-        print(option1["name"] + ", " + option1["description"] + ", " + option1["country"], option1["follower count"])
+        print(option1["name"] + ", " + option1["description"] + ", " + option1["country"], option1["follower_count"])
         print("\n")
         print("Vs")
         print("\n")
-        print(option2["name"] + ", " + option2["description"] + ", " + option2["country"], option2["follower count"])
+        print(option2["name"] + ", " + option2["description"] + ", " + option2["country"], option2["follower_count"])
 
-        a = option1["follower count"]
-        b = option2["follower count"]
+        a = option1["follower_count"]
+        b = option2["follower_count"]
 
         user_choice = a if get_user_input() == "a" else b
 
         score = compare(a, b, user_choice, score)
 
-        if score > score:
-            option1 = swap(option1, option2)
+        if (user_choice == "a" and a > b) or (user_choice == "b" and b > a):
+            option1 = option2
         else:
             print(f"Sorry, that's wrong. Final score: {score}")
             play_game = False
 
 
-play(score)
+play()
