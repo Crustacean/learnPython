e5ea822d883a0c73fef4916c5b84b4f22a488d69
Crustacean <elkyem@gmail.com>
2025-03-27 17:55:02 +0300

    adding guessing game

---
+++ appdata.py
@@ -0,0 +1,39 @@
+data=[
+    {
+        "name" : "Cardi B",
+        "description" : "a Musician",
+        "country" : "from United States",
+        "follower count" :  40
+    },
+    {
+        "name" : "Beyonce",
+        "description" : "a Musician",
+        "country" : "from United States",
+        "follower count" :  200
+    },
+    {
+        "name" : "NASA",
+        "description" : "space agency",
+        "country" : "from United States",
+        "follower count" :  30
+    },
+    {
+        "name" : "Messi",
+        "description" : "a Footballer",
+        "country" : "from Argentina",
+        "follower count" :  400
+    },
+    {
+        "name" : "Ronaldo",
+        "description" : "a Footballer",
+        "country" : "from Portugal",
+        "follower count" :  299
+    },
+    {
+        "name" : "Drake",
+        "description" : "a Musician",
+        "country" : "from Canada",
+        "follower count" :  500
+    },
+    
+]
