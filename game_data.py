a35acea4c59b43b653fee8475c3182f4c2b107fd
Crustacean <elkyem@gmail.com>
2025-03-27 20:52:54 +0300

    adding guessing game

--- game_data.py
+++ game_data.py
@@ -1,39 +1,12 @@
 data=[
-    {
-        "name" : "Cardi B",
-        "description" : "a Musician",
-        "country" : "from United States",
-        "follower count" :  40
-    },
-    {
-        "name" : "Beyonce",
-        "description" : "a Musician",
-        "country" : "from United States",
-        "follower count" :  200
-    },
-    {
-        "name" : "NASA",
-        "description" : "space agency",
-        "country" : "from United States",
-        "follower count" :  30
-    },
-    {
-        "name" : "Messi",
-        "description" : "a Footballer",
-        "country" : "from Argentina",
-        "follower count" :  400
-    },
-    {
-        "name" : "Ronaldo",
-        "description" : "a Footballer",
-        "country" : "from Portugal",
-        "follower count" :  299
-    },
-    {
-        "name" : "Drake",
-        "description" : "a Musician",
-        "country" : "from Canada",
-        "follower count" :  500
-    },
-    
+    {"name": "Instagram", "follower_count": 649, "description": "Social media platform", "country": "United States"},
+    {"name": "Cristiano Ronaldo", "follower_count": 515, "description": "Footballer", "country": "Portugal"},
+    {"name": "Ariana Grande", "follower_count": 343, "description": "Musician and actress", "country": "United States"},
+    {"name": "Dwayne Johnson", "follower_count": 339, "description": "Actor and professional wrestler", "country": "United States"},
+    {"name": "Selena Gomez", "follower_count": 337, "description": "Musician and actress", "country": "United States"},
+    {"name": "Kim Kardashian", "follower_count": 334, "description": "Television personality and businesswoman", "country": "United States"},
+    {"name": "Lionel Messi", "follower_count": 400, "description": "Footballer", "country": "Argentina"},
+    {"name": "Beyoncé", "follower_count": 282, "description": "Musician and actress", "country": "United States"},
+    {"name": "Neymar", "follower_count": 176, "description": "Footballer", "country": "Brazil"},
+    {"name": "National Geographic", "follower_count": 278, "description": "Magazine", "country": "United States"},    
 ]
